d19548168e5624d1f982581291ee0b4264ad5eb6
hapisubed <hapisubed@gmail.com>
2026-01-27 11:08:48 +0700

    Create biodata_namaKamu.py

---
+++ biodata_namaKamu.py
@@ -0,0 +1,14 @@
+print("=======================================")
+print("Program Biodata Sederhana Dengan Python")
+print("=======================================")
+
+nama = "hafidz ubaidillah"
+hobi = "makan"
+alamat = "kebondalem"
+kota = "pemalang"
+
+print("Nama Lengkap  :", nama)
+print("Alamat        :", alamat, "\n                Kota ", kota)
+print("Hobi          :", hobi)
+
+print("=======================================")

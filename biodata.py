56a3ed267ec48ce6317f6c9d8de834cf44b50999
HafidzUbaidilah <hafidzubaidillah@smkitihsanulfikri.sch.id>
2026-01-29 07:57:50 +0700

    Create biodata.py

---
+++ biodata.py
@@ -0,0 +1,21 @@
+print ("=======================================")
+print ("Program Biodata Sederhana Dengan Python")
+print ("=======================================")
+ 
+nama = "hafidz"
+hobi = "makan"
+alamat = "kebondalem"
+Kota = "pemalang"
+Propinsi = "Jawa Tengah"
+
+print("Nama Lengkap  :",nama)
+
+print("Alamat        :",alamat, "\n                Kota ", Kota, "Propinsi ",Propinsi)
+
+
+print("Hobi          :", hobi)
+
+print ("Selamat Datang di",nama )
+print ("=======================================")
+print ("=======================================")
+ 

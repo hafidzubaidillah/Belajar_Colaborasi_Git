8639449d2405d7806f0470914704cb945582d777
hapisubed <hapisubed@gmail.com>
2026-01-29 07:59:09 +0700

    Update biodata.py

--- biodata.py
+++ biodata.py
@@ -1,17 +1,17 @@
 print ("=======================================")
 print ("Program Biodata Sederhana Dengan Python")
 print ("=======================================")
- 
+ aseeeppp
 nama = "hafidz"
 hobi = "makan"
 alamat = "kebondalem"
 Kota = "pemalang"
 Propinsi = "Jawa Tengah"
-
+ervjknkl
 print("Nama Lengkap  :",nama)
 
 print("Alamat        :",alamat, "\n                Kota ", Kota, "Propinsi ",Propinsi)
-
+revaerva
 
 print("Hobi          :", hobi)
 
